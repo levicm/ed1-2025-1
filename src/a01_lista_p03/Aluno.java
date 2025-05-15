b4778712a4a944917d2d6033de136ce44a5c6191
Levi Mota <levi_127329@trt5.jus.br>
2025-05-15 20:18:24 -0300

    Aula 01

---
+++ src/a01_lista_p03/Aluno.java
@@ -0,0 +1,19 @@
+package a01_lista_p03;
+
+public class Aluno {
+	
+	private String nome;
+	private String email;
+
+	public Aluno(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return nome + " (" + email + ")";
+	}
+
+}
---
+++ src/a01_lista_p03/Lista.java
@@ -0,0 +1,43 @@
+package a01_lista_p03;
+
+public class Lista {
+
+	Aluno[] alunos = new Aluno[100];
+
+	public void adiciona(Aluno aluno) {
+		for (int i = 0; i < this.alunos.length; ++i) {
+			if (this.alunos[i] == null) {
+				this.alunos[i] = aluno;
+				break;
+			}
+		}
+	}
+	
+	public int tamanho() {
+		for (int i = 0; i < this.alunos.length; ++i) {
+			if (this.alunos[i] == null) {
+				return i;
+			}
+		}
+		return this.alunos.length;
+	}
+
+	@Override
+	public String toString() {
+		String result = "[";
+		// Componho a string com o conteúdo desejado
+		for (int i = 0; i < alunos.length; ++i) {
+			if (alunos[i] != null) {
+				if (i > 0) {
+					result += ", ";
+				}
+				result += alunos[i].toString();
+			} else {
+				break;
+			}
+		}
+		result += "]";
+		return result;
+	}
+
+}
