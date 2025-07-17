cb7c3b218c56d4754cfd4e8bbc56064c79bbfff6
Levi Mota <levi.mota@gmail.com>
2025-07-17 20:29:26 -0300

    Busca ordenada

---
+++ src/a07_busca_ordenada_p01/ListaOrdenada.java
@@ -0,0 +1,103 @@
+package a07_busca_ordenada_p01;
+
+public class ListaOrdenada {
+
+	Object[] elementos = new Object[20];
+	private int tamanho = 0;
+
+	public void adiciona(Comparable elemento) {
+		garanteEspaco();
+		if (tamanho == 0) {
+			elementos[0] = elemento;
+			tamanho++;
+		} else {
+			for (int i = 0; i < tamanho; ++i) {
+				if (elemento.compareTo((Comparable) elementos[i]) < 0) {
+					adiciona(elemento, i);
+					return;
+				}
+			}
+		}
+	}
+	
+	private void adiciona(Comparable aluno, int posicao) {
+		garanteEspaco();
+		if (posicao <= tamanho) {
+			// desloca elementos posteriores à posição
+			for (int i = tamanho; i > posicao; --i) {
+				elementos[i] = elementos[i - 1];
+			}
+			elementos[posicao] = aluno;
+			tamanho++;
+		}
+	}
+	
+	private void garanteEspaco() {
+		// Testar se precisa aumentar o espaço
+		if (tamanho == elementos.length) {
+			// Aumentar o espaço (dobrar o tamanho do array)
+			int novoTamnho = elementos.length * 2;
+			System.out.println("Aumentando o tamanho do array para " + novoTamnho);
+			Object[] novoArray = new Object[novoTamnho];
+			for (int i = 0; i < elementos.length; ++i) {
+				novoArray[i] = elementos[i];
+			}
+			this.elementos = novoArray;
+		}
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+	
+	public Object pega(int posicao) {
+		// Testa se a posição é válida
+		if (posicao < elementos.length) {
+			return elementos[posicao];
+		}
+		return null;
+	}
+	
+	public void remove(int posicao) {
+		if (posicao < tamanho) {
+			for (int i = posicao; i < tamanho; ++i) {
+				elementos[i] = elementos[i + 1];
+			}
+		}
+	}
+
+	public int busca(Comparable elemento) {
+		int resultado = -1;
+		if (elemento != null) {
+			for (int i = 0; i < tamanho; ++i) {
+				System.out.println("Buscanco na posição " + i + " de " + tamanho);
+				if (elemento.compareTo(this.elementos[i]) == 0) {
+					resultado = i;
+					break;
+				} else if  (elemento.compareTo(elementos[i]) < 0) {
+					break;
+				}
+			}
+		}
+		return resultado;
+	}
+
+	@Override
+	public String toString() {
+		String result = "[";
+		// Componho a string com o conteúdo desejado
+		for (int i = 0; i < elementos.length; ++i) {
+			if (elementos[i] != null) {
+				if (i > 0) {
+					result += ", ";
+				}
+				result += elementos[i].toString();
+			} else {
+				break;
+			}
+		}
+		result += "]";
+		return result;
+	}
+
+}
---
+++ src/a07_busca_ordenada_p01/TesteAdiciona.java
@@ -0,0 +1,21 @@
+package a07_busca_ordenada_p01;
+
+public class TesteAdiciona {
+
+	public static void main(String[] args) {
+		ListaOrdenada lista = new ListaOrdenada();
+		System.out.println(lista);
+
+		lista.adiciona(new Aluno("Pedro", "pedro@gmail.com", 8.5));
+		lista.adiciona(new Aluno("Maria", "maria@gmail.com", 7.1));
+		lista.adiciona(new Aluno("Joana", "joana@gmail.com", 7.5));
+		System.out.println(lista);
+
+		lista.adiciona(new Aluno("Maria", "maria@gmail.com", 9.0));
+		lista.adiciona(new Aluno("Carlos", "carlos@gmail.com", 6.0));
+		lista.adiciona(new Aluno("Maria", "maria@gmail.com", 4.0));
+		
+		System.out.println(lista);
+	}
+
+}
